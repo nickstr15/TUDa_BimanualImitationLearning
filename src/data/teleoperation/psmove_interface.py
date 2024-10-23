e707673652b449954b93b58f9cd02908616f12e0
nickstr15 <nickstriebel@gmx.de>
2024-10-23 19:03:24 +0200

    psmove connection working

--- src/data/teleoperation/psmove_interface.py
+++ src/data/teleoperation/psmove_interface.py
@@ -1,17 +1,13 @@
-import ctypes
 import sys
 import os
+import time
 
 import yaml
 
-import time
-import numpy as np
-import threading
 
 ######################################################################################
 # Setup PSMove #######################################################################
 ######################################################################################
-
 full_config_path = os.path.join(os.path.dirname(__file__), "psmove_config.yml")
 with open(full_config_path, 'r') as f:
     PSMOVE_CONFIG = yaml.safe_load(f)
@@ -20,23 +16,58 @@ PSMOVE_API_PATH = PSMOVE_CONFIG["psmove_api_path"]
 LEFT_ADDRESS = PSMOVE_CONFIG["left_controller_address"]
 RIGHT_ADDRESS = PSMOVE_CONFIG["right_controller_address"]
 
-lib_path = os.path.join(PSMOVE_API_PATH, "lib")
-bindings_path = os.path.join(PSMOVE_API_PATH, 'bindings', 'python')
-additional_path = os.path.join(PSMOVE_API_PATH, 'additional')
-
-if 'PSMOVEAPI_LIBRARY_PATH' not in os.environ:
-    os.environ['PSMOVEAPI_LIBRARY_PATH'] = lib_path
-
-if 'LD_LIBRARY_PATH' not in os.environ:
-    os.environ['LD_LIBRARY_PATH'] = lib_path + ":" + bindings_path + ":" + additional_path
-else:
-    os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH'] + ":" + lib_path + ":" + bindings_path + ":" + additional_path
-
-sys.path.insert(0, lib_path)
-sys.path.insert(0, bindings_path)
+sys.path.insert(0, PSMOVE_API_PATH)
 
 import psmove
 ######################################################################################
+
+class PSMoveInterface:
+
+    def __init__(self, frequency=20.0):
+        self.dt = 1.0/frequency
+        self._left_controller = None
+        self._right_controller = None
+
+    def start(self):
+        self._identify_controllers()
+
+    def _identify_controllers(self):
+        move_count = psmove.count_connected()
+
+        if move_count == 0:
+            raise Exception("No controllers connected")
+
+        for idx in range(move_count):
+            controller = psmove.PSMove(idx)
+            controller_serial = controller.get_serial()
+
+            if controller_serial == LEFT_ADDRESS:
+                print("Left controller found: ", controller_serial)
+                self._left_controller = controller
+                self._left_controller.set_leds(0, 0, 255)
+                self._left_controller.update_leds()
+            elif controller_serial == RIGHT_ADDRESS:
+                print("Right controller found: ", controller_serial)
+                self._right_controller = controller
+                self._right_controller.set_leds(255, 0, 0)
+                self._right_controller.update_leds()
+            else:
+                raise Exception(f"Unknown controller with serial: {controller_serial}." + \
+                                "Please update the config file with the correct serials.")
+
+    def stop(self):
+        ...
+
+
+if __name__ == "__main__":
+    psmove_interface = PSMoveInterface()
+    psmove_interface.start()
+    time.sleep(10)
+    psmove_interface.stop()
+
+
+
+
 
 
 
