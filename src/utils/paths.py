4436f0f4a5ad37993daa0a84b59c3203cba3f278
nickstr15 <nickstriebel@gmx.de>
2024-11-15 15:54:10 +0100

    fix folder naming

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -3,7 +3,7 @@ import os
 PROJECT_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir))
 
 CONTROL_CONFIGS_DIR = os.path.join(PROJECT_ROOT_DIR, "src", "control", "control_configs")
-DEMOS_DIR = os.path.join(PROJECT_ROOT_DIR, "demonstration")
+DEMOS_DIR = os.path.join(PROJECT_ROOT_DIR, "demonstrations")
 ENVIRONMENTS_DIR = os.path.join(PROJECT_ROOT_DIR, "src", "environments")
 RECORDING_DIR = os.path.join(PROJECT_ROOT_DIR, "recordings")
 SCENES_DIR = os.path.join(PROJECT_ROOT_DIR, "src", "environments", "scenes")
