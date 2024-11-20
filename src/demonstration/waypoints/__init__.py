83d75c27259943df40d1b262d15eca36841693e9
nickstr15 <nickstriebel@gmx.de>
2024-11-20 20:30:05 +0100

    update ENV_TO_WAYPOINT_EXPERT-dict

--- src/demonstration/waypoints/__init__.py
+++ src/demonstration/waypoints/__init__.py
@@ -1,5 +1,15 @@
+from src.demonstration.waypoints.two_arm_handover_wp_expert import TwoArmHandoverWaypointExpert
+from src.demonstration.waypoints.two_arm_lift_wp_expert import TwoArmLiftWaypointExpert
+from src.demonstration.waypoints.two_arm_peg_in_hole_wp_expert import TwoArmPegInHoleWaypointExpert
 from src.demonstration.waypoints.two_arm_pick_place_wp_expert import TwoArmPickPlaceWaypointExpert
+from src.demonstration.waypoints.two_arm_quad_insert_wp_expert import TwoArmQuadInsertWaypointExpert
+from src.demonstration.waypoints.two_arm_transport_wp_expert import TwoArmTransportWaypointExpert
 
 ENV_TO_WAYPOINT_EXPERT = {
-    "TwoArmPickPlace": TwoArmPickPlaceWaypointExpert
+    "TwoArmHandover": TwoArmHandoverWaypointExpert,
+    "TwoArmLift": TwoArmLiftWaypointExpert,
+    "TwoArmPegInHole": TwoArmPegInHoleWaypointExpert,
+    "TwoArmPickPlace": TwoArmPickPlaceWaypointExpert,
+    "TwoArmQuadInsert": TwoArmQuadInsertWaypointExpert,
+    "TwoArmTransport": TwoArmTransportWaypointExpert,
 }
