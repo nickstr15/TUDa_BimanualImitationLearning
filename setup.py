76e053cb3eb764b6fd2b212fd249a4cf542e0308
nickstr15 <nickstriebel@gmx.de>
2025-01-14 16:28:46 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='bil',
+    version='0.0.1',
+    author='Nick Striebel',
+    author_email='nick.striebel@stud.tu-darmstadt.de',
+    packages=['robomimic_ext', 'robosuite_ext', 'utils'],
+)
+
