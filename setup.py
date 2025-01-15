d2cede4ac914fb3a6c7638293e29ba40cac75f72
nickstr15 <nickstriebel@gmx.de>
2025-01-15 08:43:23 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+from codecs import open
+from os import path
+
 from setuptools import setup
+
+folder = path.abspath(path.dirname(__file__))
+requires_list = []
+with open(path.join(folder, 'requirements.txt'), encoding='utf-8') as f:
+    for line in f:
+        requires_list.append(str(line))
 
 setup(
     name='bil',
@@ -6,5 +15,6 @@ setup(
     author='Nick Striebel',
     author_email='nick.striebel@stud.tu-darmstadt.de',
     packages=['robomimic_ext', 'robosuite_ext', 'utils'],
+    install_requires=requires_list,
 )
 
